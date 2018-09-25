fff05b083a1163c34d4ee2a196f9d0ff07c39a12
Anna Gabriela <agmc0997@gmail.com>
2018-09-25 11:45:40 -0300

    add dotenv

--- db/settings.py
+++
@@ -1,7 +0,0 @@
-import redis
-
-host = "redis-18369.c16.us-east-1-3.ec2.cloud.redislabs.com"
-port = 18369
-password = "$123"
-
-database = redis.Redis(host=host, port=port, password=password, decode_responses=True)

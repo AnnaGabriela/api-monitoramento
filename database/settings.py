dbb5a88fa24102f54ab9faebb8fac2bfe435f302
Anna Gabriela <agmc0997@gmail.com>
2018-09-26 12:41:43 -0300

    change env vars

--- database/settings.py
+++ database/settings.py
@@ -1,8 +1,8 @@
 import os
 import redis
 
-HOST = os.environ.get("HOST")
-PORT = os.environ.get("PORT")
-PASSWORD = os.environ.get("PASSWORD")
+HOST = os.environ.get("REDIS_HOST")
+PORT = os.environ.get("REDIS_PORT")
+PASSWORD = os.environ.get("REDIS_PASSWORD")
 
 db = redis.Redis(host=HOST, port=PORT, password=PASSWORD, decode_responses=True)
